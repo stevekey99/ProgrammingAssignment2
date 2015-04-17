174d411c71caafb2abc4af61c0bb9313a99d4da9
stevekey99 <stevekey99@yahoo.com>
2015-04-17 08:45:51 -0600

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix accepts a matrix argument and caches the input matrix into the parent environment
 
-## Write a short comment describing this function
+
+## Used the guidelines of the cachemean example and replaced mean with matrix and used function(solve) 
+## rather than function(mean)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <-NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(solve) m<<-solve
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)    
 }
 
 
-## Write a short comment describing this function
+## Function returns a matrix that is the inverse of the input matrix from cache if it exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
